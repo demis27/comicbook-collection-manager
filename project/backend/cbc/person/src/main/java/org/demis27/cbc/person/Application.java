5232343c8fb421f50fb64b70ef701216ab896129
Kermabon Stéphane <skermabon@talend.com>
2019-10-23 11:31:44 +0200

    Create maven project

---
+++ project/backend/cbc/person/src/main/java/org/demis27/cbc/person/Application.java
@@ -0,0 +1,10 @@
+package org.demis27.cbc.person;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
